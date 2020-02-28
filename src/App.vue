7d9e391493f3c530254a997e7e75623bc4f557ab
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-02-28 15:19:45 +0100

    Flex Grid

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+
+
+        <div id="app">
+    <h1 id="title">F1 2020 GRID </h1>
+
+    <test></test>
+
+  </div>
+
+
+</template>
+
+<script>
+
+  import  test from './components/test.vue'
+
+
+  export default {
+    name: 'App',
+    components: {
+      test
+
+
+
+    }
+  }
+</script>
+
+<style>
+
+  body {
+    background-color: #19161F;
+  }
+
+  @font-face {
+    font-family: F1Font;
+    src: url("assets/Formula1-Regular_web_0.ttf");
+  }
+
+  #title {
+    font-family: F1Font,sans-serif;
+    color: red;
+    text-align: center;
+  }
+
+
+
+
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,104 @@
+<template>
+    <div>
+        <v-container fluid grid-list-md>
+
+
+        <v-flex d-flex>
+
+        <v-layout row jusitfy-center>
+
+
+        <v-flex 2 id="hello" v-for="team in teams" v-bind:key="team">
+            <div id="card">
+                <img v-bind:src="team.img" v-bind:alt="team.name" id="team">
+                <p id="name"> {{ team.name }} </p>
+                <p id="engine"> {{ team.engine }} </p>
+            </div>
+
+        </v-flex>
+        </v-layout>
+        </v-flex>
+        </v-container>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "test",
+        data : function () {
+            return {
+                teams : [
+
+                    { name : "Mercedes AMG Petronas Formula One Team", car : "W11", engine : "Mercedes" , img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/mercedes.png"},
+                    { name : "Scuderia Ferrari Mission Winnow", car : "SF1000", engine : "Ferrari", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/ferrari.png"},
+                    { name : "Aston Martin Red Bull Racing", car : "RB16",  engine : "Honda", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/redbull.png"},
+
+                    { name : "McLaren F1 Team", car : "MCL35", engine : "Renault", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/mclaren.png"},
+                    { name : "Renault F1 Team", car : "RS20", engine : "Renault", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/renault.png"},
+
+                    { name : "Scuderia AlphaTauri Honda", car : "AT01", engine : "Honda", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/alphatauri.png"},
+                    { name : "BWT Racing Point F1 Team", car : "RP20", engine : "Mercedes", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/racingpoint.png"},
+                    { name : "Alfa Romeo Racing Orlen", car : "C39", engine : "Ferrari", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/alfaromeo.png"},
+                    { name : "Haas F1 Team", car : "VF-20", engine : "Ferrari", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/haas.png"},
+
+                    { name : "ROKiT Williams Racing", car : "FW43", engine : "Mercedes", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/williams.png"},
+
+
+                ]
+            }
+
+        }
+
+
+    }
+
+
+</script>
+
+<style scoped>
+
+    #card {
+        height: 150px;
+        width: 300px;
+        background-color: #555;
+        border-radius: 10px;
+        text-align: center;
+
+
+
+
+    }
+
+    #card:hover {
+        background-color: red;
+        transition: 0.5s ease;
+        cursor: pointer;
+    }
+
+    #name {
+        font-weight: bold;
+        margin-top: 5px;
+        font-family: F1Font, sans-serif;
+        font-size: 20px;
+
+    }
+
+    #engine {
+        font-family: F1Font, sans-serif;
+        margin-top: 5px;
+        font-weight: bold;
+    }
+
+    #team {
+        margin-top: 10px;
+    }
+
+    #hello {
+        margin-top: 10px;
+        display: block;
+    }
+
+
+
+</style>
