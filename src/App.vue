249e62bf96796939157db5bcdfad8a4e5a8b57c3
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-03-01 20:24:00 +0100

    Functional Interface

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,70 @@
 <template>
 
 
-        <div id="app" v-bind:style="{ backgroundColor: activeColor}">
-    <h1 id="title">F1 2020 GRID </h1>
-            <button @click="darktheme">Black theme</button>
+    <div id="app" v-bind:style="{ backgroundColor: activeColor}">
+        <h1 id="title">F1 2020 GRID </h1>
+        <div id="test">
 
-    <test></test>
-    <chart></chart>
+        </div>
+        <v-btn v-bind:style="{color : textColor}" v-bind:dark="true" color="rgb(37,37,37)" right="true" @click="darktheme">{{ theme }}</v-btn>
 
-  </div>
+        <test></test>
+
+        <div id="blank">
+
+        </div>
+
+
+    </div>
 
 
 </template>
 
 <script>
-
-  import  test from './components/test.vue'
-
-
-
-  export default {
-    name: 'App',
-    components: {
-      test,
-
-    },
-      data() {
-        return {
-            activeColor : 'white'
-        }
-      },
-
-      methods : {
-
-        darktheme : function () {
-
-            if (this.activeColor === 'white'){
-
-                this.activeColor = '#252525'
+    import  test from './components/test.vue'
+    export default {
+        name: 'App',
+        components: {
+            test,
+        },
+        data() {
+            return {
+                activeColor : 'white',
+                textColor :  'white',
+                theme : 'Dark theme'
             }
-            else {
-                this.activeColor = 'white'
+        },
+        methods : {
+            darktheme : function () {
+                if (this.activeColor === 'white'){
+
+                    this.activeColor = '#252525';
+                    this.textColor = 'white';
+                    this.theme = 'Light Theme';
+                }
+                else {
+                    this.activeColor = 'white';
+                    this.textColor = 'white';
+                    this.theme = 'Dark Theme'
+                }
             }
-
         }
-      }
-
-  }
-
-
-
-
+    }
 </script>
 
 <style>
+    @font-face {
+        font-family: F1Font;
+        src: url("assets/Formula1-Regular_web_0.ttf");
+    }
+    #title {
+        font-family: F1Font,sans-serif;
+        color: red;
+        text-align: center;
+    }
 
-
-
-  @font-face {
-    font-family: F1Font;
-    src: url("assets/Formula1-Regular_web_0.ttf");
-  }
-
-
-
-  #title {
-    font-family: F1Font,sans-serif;
-    color: red;
-    text-align: center;
-  }
-
-
-
-
-
+    #blank {
+        height: 100px;
+    }
 
 </style>
--- src/components/test.vue
+++ src/components/test.vue
@@ -2,7 +2,7 @@
     <div>
 
         <v-carousel class="diapo"
-        :show-arrows="false">
+                    :show-arrows="false">
             <v-carousel-item
                     v-for="(diapo,i) in diapos "
                     :key="i"
@@ -38,10 +38,18 @@
             </v-flex>
         </v-container>
 
-        <button @click="showOthers">TOP TEAM</button>
-        <button @click="showBests">Others</button>
-        <button @click="ShowMain">Show</button>
-        <button @click="DeleteMain">Delete</button>
+        <div class="buttons-main">
+            <v-btn @click="ShowMain" class="buttons-component">Show</v-btn>
+            <v-btn @click="DeleteMain" class="buttons-component">Delete</v-btn>
+            <v-btn @click="showOthers" v-bind:style="{ visibility : showButtons}" class="buttons-component">TOP TEAMS</v-btn>
+            <v-btn @click="showBests" v-bind:style="{ visibility : showButtons}" class="buttons-component">Others</v-btn>
+
+
+        </div>
+
+
+
+
 
 
 
@@ -52,28 +60,26 @@
 
         <v-container fluid grid-list-md v-if="read">
 
-        <v-flex d-flex>
+            <v-flex d-flex>
 
-            <v-layout row jusitfy-center>
-
-
-                <v-flex 4 id="hello" v-for="team in topTeams" v-bind:key="team">
-                    <div class="card">
-                        <img v-bind:src="team.img" v-bind:alt="team.name" class="team">
-                        <p class="name"> {{ team.name }} </p>
-                        <p class="engine"> {{ team.car }} - {{ team.engine }}</p>
-                    </div>
+                <v-layout row jusitfy-center>
 
 
+                    <v-flex 4 id="hello" v-for="team in topTeams" v-bind:key="team">
+                        <div class="card">
+                            <img v-bind:src="team.img" v-bind:alt="team.name" class="team">
+                            <p class="name"> {{ team.name }} </p>
+                            <p class="engine"> {{ team.car }} - {{ team.engine }}</p>
+                        </div>
 
-                </v-flex>
-            </v-layout>
-        </v-flex>
+
+
+                    </v-flex>
+                </v-layout>
+            </v-flex>
         </v-container>
 
-        <div id="chart">
-            <canvas id="ranking"></canvas>
-        </div>
+
     </div>
 
 
@@ -89,7 +95,6 @@
                     { name : "Mercedes AMG Petronas Formula One Team", car : "W11", engine : "Mercedes" , img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/mercedes.png" , top : true},
                     { name : "Scuderia Ferrari Mission Winnow", car : "SF1000", engine : "Ferrari", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/ferrari.png", top : true},
                     { name : "Aston Martin Red Bull Racing", car : "RB16",  engine : "Honda", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/redbull.png", top : true},
-
                     { name : "McLaren F1 Team", car : "MCL35", engine : "Renault", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/mclaren.png", top : false},
                     { name : "Renault F1 Team", car : "RS20", engine : "Renault", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/renault.png",top : false},
                     { name : "Scuderia AlphaTauri Honda", car : "AT01", engine : "Honda", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/alphatauri.png",top : false},
@@ -97,116 +102,66 @@
                     { name : "Alfa Romeo Racing Orlen", car : "C39", engine : "Ferrari", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/alfaromeo.png",top : false},
                     { name : "Haas F1 Team", car : "VF-20", engine : "Ferrari", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/haas.png",top : false},
                     { name : "ROKiT Williams Racing", car : "FW43", engine : "Mercedes", img : "https://d3qmgl49lqmtd9.cloudfront.net/live/tst18d2/williams.png",top : false},
-                    { name : "W11 - Defending champion car 2019 ", car : "W10", engine : "Mercedes", img : "https://cdn-8.motorsport.com/images/vcl/K6xbRj2Q/s3/img992460015-1.png",top : false},
+                    { name : "W11 - Defending champion car 2019 ", car : "W10", engine : "Mercedes", img : "https://cdn-8.motorsport.com/images/vcl/K6xbRj2Q/s3/img992460015-1.png",top : true},
                     { name : "FW42 - Wooden Spoon 2019", car : "FW42", engine : "Mercedes", img : "https://cdn-9.motorsport.com/images/vcl/n0mZ4g2z/s3/img1339655169-1.png",top : false},
-
-
                 ],
-
                 diapos : [
                     { picture : "https://www.formula1.com/content/dam/fom-website/manual/Misc/2020/Car-launches/Mercedes/M226201.jpg" },
                     { picture: "https://autodius.com/wp-content/uploads/2020/02/ferrari-sf1000-f1.jpg"},
                     { picture : "https://img.sportauto.fr/news/2020/02/12/1546372/855e542dfddd26a815025d71-1920-1280.jpg"},
-
                     { picture : "https://www.formula1.com/content/dam/fom-website/manual/Misc/2020/Car-launches/McLaren/LandoNorris2020/2020_MCL35_LN4_3Q_NO_BAT_1.jpg"},
                     { picture : "https://img.sportauto.fr/news/2020/02/19/1546595/1fc973a1bd49bd2ef75caf8c-1920-1280.jpg"},
                     { picture : "https://img.sportauto.fr/news/2020/02/06/1546247/ddce8b5e8d7238a389063889-1920-1280.jpg"},
-
                     { picture : "https://img.autoplus.fr/picture/alfa_romeo/c39/1546486/Alfa_Romeo_C39_2020_165b9-1200-800.jpg"},
                     { picture : "https://www.formula1.com/content/dam/fom-website/manual/Misc/2020/Testing2020/Day1/GettyImages-1201880467.jpg"},
                     { picture : "https://img.autoplus.fr/news/2020/02/17/1546530/22391467fb64628f9db34246-1200-800.jpg"},
-
                     { picture : "https://img.autoplus.fr/news/2020/02/14/1546501/0430cd7aa33f0c7c34f79011-1200-800.jpg"},
-
-
-
-
-
                 ],
-
                 first : '',
                 order : false,
                 read : false,
+                showButtons : 'hidden'
             }
         },
-
         computed : {
-
             // eslint-disable-next-line vue/return-in-computed-property
             topTeams() {
-
                 if (this.order === true) {
-
                     return this.teams.filter(team =>  team.top)
-
                 }
-
                 else {
-
                     return this.teams.filter(team => !  team.top)
-
-
                 }
-
-
             }
-
-
         },
-
         methods : {
-
             showOthers : function () {
-
-
-
-
                 this.first = 'The Top Teams';
                 this.order = true
-
-
             },
-
             showBests : function () {
-
                 this.first = 'The Others';
                 this.order = false
-
-
             },
-
-
             ShowMain : function () {
-
                 this.read = true;
                 this.first = 'The Top Teams';
-                this.order = true
-
+                this.order = true;
+                this.showButtons = 'visible'
             },
-
             DeleteMain : function () {
-
                 this.read = false;
                 this.first = '';
-                this.order = false
-
+                this.order = false;
+                this.showButtons = 'hidden'
             }
-
-
-
-
-
         }
     }
-
-
     /*
     TO DO :
-
     Filter ( Only 3 Top Teams )
     V - if
     overlay div ?
-
      */
 </script>
 
@@ -218,11 +173,7 @@
         border-radius: 10px;
         text-align: center;
     }
-    .card:hover {
-        background-color: red;
-        transition: 0.5s ease;
-        cursor: pointer;
-    }
+
     .name {
         font-weight: bold;
         margin-top: 5px;
@@ -241,25 +192,24 @@
         margin-top: 10px;
         display: block;
     }
-
     .subtitle {
         margin-top: 20px;
         font-family: F1Font,sans-serif;
         color: red;
         text-align: center;
     }
-
     .diapo {
-
         margin-top: 10px;
     }
 
+    .buttons-component {
+        margin-top: 10px;
+        margin-left: 10px;
+    }
 
-
-
-
-
-
+    .buttons-main {
+        text-align: center;
+    }
 
 
 </style>
